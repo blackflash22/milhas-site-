6612f50538b85e892e5e181b196f6ee01b36d364
RM Partiu Viagens <contato@extraordinariotrip.com.br>
2026-08-16 18:33:22 -0300

    Configure static export for GitHub Pages

---
+++ dist/_next/static/chunks/app/(onertravel)/onertravel/pacotes/page-da8afaf701a03571.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[131],{},function(n){n.O(0,[759,606,971,117,744],function(){return n(n.s=5606)}),_N_E=n.O()}]);
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  distDir: "dist",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
